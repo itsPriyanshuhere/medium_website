7fea8d6733c5ce5e3b610b3fd6bbeed8da360532
Priyanshu Pandey <priyanshupandey@Priyanshus-MacBook-Air.local>
2024-09-13 19:33:05 +0530

    created routes

--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -3,6 +3,12 @@ import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import {sign, verify} from 'hono/jwt';  
 
+interface PostData {
+    title: string;
+    content: string;
+    id?: string;
+  }
+
 
 const blogRouter = new Hono<{
   Bindings:{
@@ -37,37 +43,104 @@ blogRouter.use('/*', async (c, next) => {
 
 });
 
-blogRouter.get('blog/:id', (c) => {
+blogRouter.post('/', async (c) => {
 
-    return c.text('hello');
-})
+    //@ts-ignore
+    const userId = c.get('userId');
 
-blogRouter.post('blog', async(c) => {
-
-    const body = await c.req.json();
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL,
-    }).$extends(withAccelerate())   ;
+    }).$extends(withAccelerate());
 
-    await prisma.post.create({
+    const body = await c.req.json<PostData>();
+
+    const post = await prisma.post.create({
         data:{
             title: body.title,
             content: body.content,
-            authorId: '1'
+            authorId: userId as string
         }
-    })
+    });
+    return c.json({
+        id: post.id
+    });
+});
 
-    return c.text('hello');
-})
-
-blogRouter.get('/signin', async(c) => {
+blogRouter.put('/', async (c) => {
     //@ts-ignore
-    console.log(c.get('userId'));
-    return c.text('working');
-})
+    const userId = c.get('userId');
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
 
-blogRouter.put('blog', (c) => {
-    return c.text('hello');
-})
+    const body = await c.req.json<PostData>();
+
+    try {
+    await prisma.post.update({
+        where: {
+            //@ts-ignore
+            id: body.id,
+            authorId: userId as string
+        },
+        data: {
+            title: body.title,
+            content: body.content
+        }
+    });
+
+    c.status(200);
+    return c.json({
+        message: 'Updated the blog'
+    });
+}
+    catch(e){
+        return c.json({
+            error: e
+        }, 500);
+    }
+});
+
+blogRouter.get('/bulk', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const posts = await prisma.post.findMany({});
+    return c.json({ posts });
+});
+
+blogRouter.get('/:id', async (c) => {
+    const id = c.req.param('id');
+    console.log(id);
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try{
+    const post = await prisma.post.findUnique({
+        where:{
+            id
+        },
+    });
+
+    if (!post) {
+        return c.json({ error: 'Post not found' }, 500);
+    }
+
+    return c.json({post});
+}
+    catch(e){
+        return c.json({
+        error: e
+        })
+}
+
+    
+});
+
+
 
 export default blogRouter;
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcyN2IyOWZiLTgwZDgtNDMxMy05NzZmLTNhN2U2MGNjM2MzZSJ9.c7qWet3v2bO2KQ-2JjvPYBrn0rZzOZWTYOZ5cJmCNd4
+//6e1bcabf-266b-4b7f-9465-474e51970ecc
